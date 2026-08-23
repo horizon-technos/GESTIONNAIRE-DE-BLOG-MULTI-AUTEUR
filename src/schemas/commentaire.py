e92dd105af990b7c8a83c53d11aa403770eb0125
manga237 <brayanemanga126@gmail.com>
2026-08-23 03:10:49 +0100

    ajout des classes commentaires et tag

---
+++ src/schemas/commentaire.py
@@ -0,0 +1,9 @@
+class Commentaire:
+ def __init__(self,id,contenu,id_post,id_auteur,id_parent,created_at):
+  self.id=id
+  self.contenu=contenu
+  self.id_post=id_post
+  self.id_auteur=id_auteur
+  self.id_parent=id_parent
+  self.created_at=created_at
+  
---
+++ src/schemas/tag.py
@@ -0,0 +1,6 @@
+class Tag:
+  def __init__(self,id,nom,slug,description):
+    self.id=id
+    self.nom=nom
+    self.slug=slug
+    self.description=description
